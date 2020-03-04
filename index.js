162efee1b85b78923d5a702ab420c42693125be2
Asydius <ivan.aca93@gmail.com>
2020-03-04 16:56:41 -0600

    Version 1.0

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running')
+app.get('/',(req,res,next) => {
+    res.send("Bienvenido")
+})
+
+app.get('/:name',(req, res, next) => {
+    console.log(req.params.name)
+    res.send("Hola, " + req.params.name)
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running")
 })
