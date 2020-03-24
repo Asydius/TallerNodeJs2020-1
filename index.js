6988f92d55e90b25dff1c4170eed45909cedbcee
Asydius <ivan.aca93@gmail.com>
2020-03-24 14:29:45 -0600

    Add RegExp for int rutes // Add pokedex.json

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const app = express();
+const {pokemon} = require('./pokedex.json');
 
 app.get('/',(req,res,next) => {
-    res.send("Bienvenido")
+    res.status(200);
+    res.send("Bienvenido al Pokedex")
 })
 
-app.get('/:name',(req, res, next) => {
-    console.log(req.params.name)
-    res.send("Hola, " + req.params.name)
+app.get('/pokemon/all',(req, res, next) => {
+    res.status(200);
+    res.send(pokemon);
+});
+
+app.get('/pokemon/:id([0-9]{1,3})',(req, res, next) => {
+    const id = req.params.id-1;
+    if(id >= 0 && id <= 150){
+        res.status(200);
+        res.send(pokemon[req.params.id-1]);
+    }
+    else{
+        res.status(404);
+        res.send("Pokemon no encontrado");
+    }
+});
+
+app.get('/pokemon/:name',(req, res, next) => {
+    const name = req.params.name;
+    for(x = 0; x < pokemon.lenght; x++){
+        if(pokemon[i].name == name){
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado");
 });
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running")
+    console.log("Server is running...")
 })
